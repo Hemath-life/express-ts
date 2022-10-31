0551805fe8b44f1362a64251a301780988a057ca
Hemath <hemathkumar@bitcot.com>
2022-10-31 10:28:48 +0530

    typo

--- utils/handlers.ts
+++ utils/handlers.ts
@@ -1,25 +1,9 @@
-import { NextFunction } from 'express';
-import { Response, Request } from 'express';
-// export class error {
-//     message!: string;
-//     status!: number;
-//     additionalInfo!: any;
-
-//     constructor(message: string, status: number = 500, additionalInfo: any = {}) {
-//         this.message = message;
-//         this.status = status;
-//         this.additionalInfo = additionalInfo;
-//     }
-// }
 /**
  * Custom error handler to standardize error objects returned to
  * the client
- *
  * @param err Error caught by Express.js
- * @param req Request object provided by Express
- * @param res Response object provided by Express
- * @param next NextFunction function provided by Express
  */
+
 function ErrorHandler(err: any): any {
     let error = err;
 
@@ -31,10 +15,8 @@ function ErrorHandler(err: any): any {
         } catch (e) {
             message = undefined;
         }
-        // we are not using the next function to prvent from triggering
-        // the default error-handler. However, make sure you are sending a
-        // response to client to prevent memory leaks in case you decide to
-        // NOT use, like in this example, the NextFunction .i.e., next(new Error())
+        // we are not using the next function to prevent from triggering
+        // the default error-handler.
     } else if (typeof error === 'string' || error instanceof String) {
         message = error;
     }
