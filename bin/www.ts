8009738baa3e2f3bea54ff9507da653a60be2b8a
Hemath <hemathkumar@bitcot.com>
2022-11-19 11:32:53 +0530

    log set up

--- bin/www.ts
+++ bin/www.ts
@@ -24,10 +24,42 @@ connectDB()
         process.exit(1);
     });
 
+const colors = {
+    reset: '\x1b[0m',
+    bright: '\x1b[1m',
+    dim: '\x1b[2m',
+    underscore: '\x1b[4m',
+    blink: '\x1b[5m',
+    reverse: '\x1b[7m',
+    hidden: '\x1b[8m',
+    fg: {
+        black: '\x1b[30m',
+        red: '\x1b[31m',
+        green: '\x1b[32m',
+        yellow: '\x1b[33m',
+        blue: '\x1b[34m',
+        magenta: '\x1b[35m',
+        cyan: '\x1b[36m',
+        white: '\x1b[37m',
+        crimson: '\x1b[38m', // Scarlet
+    },
+    bg: {
+        black: '\x1b[40m',
+        red: '\x1b[41m',
+        green: '\x1b[42m',
+        yellow: '\x1b[43m',
+        blue: '\x1b[44m',
+        magenta: '\x1b[45m',
+        cyan: '\x1b[46m',
+        white: '\x1b[47m',
+        crimson: '\x1b[48m',
+    },
+};
+
 var exLog = console.log;
-const d: string = new Date().toLocaleTimeString()
-console.log = function (msg) {
-    var timestamp = `[${d}]  `;
+const d: string = new Date().toLocaleTimeString();
+console.log = function () {
+    var timestamp = `${colors.reverse}[${d}]${colors.reset}  `;
     Array.prototype.unshift.call(arguments, timestamp);
     exLog.apply(this, [...arguments]);
 };
